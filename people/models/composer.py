74be1a75a1fd80013504eec5f2c8767a9a24cd7a
Adrian Smallwood <smallwoodadrian54@gmail.com>
2020-02-21 18:50:16 -0800

    Create composer and performer models

---
+++ people/models/composer.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Composer(models.Model):
+    given_name = models.CharField(max_length=100)
+    family_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.family_name}, {self.given_name}'
+
+    @property
+    def full_name(self):
+        return f'{self.given_name} {self.family_name}'
+
+
